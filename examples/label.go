fd0fc392237138abf995a55bf629b563d9859cbc
Amir Omidi <amir@aaomidi.com>
2023-03-29 21:43:14 -0400

    Use flags for the sample Go code (#17)

--- examples/label.go
+++ examples/label.go
@@ -3,8 +3,11 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/base32"
+	"flag"
 	"fmt"
 )
+
+var label = flag.String("label", "https://example.com/acme/acct/ExampleAccount", "Label used to calculate the DNS-ACCOUNT-01 value with.")
 
 func CalculateLabel(accountURI string) string {
 	x := sha256.Sum256([]byte(accountURI))
@@ -13,5 +16,7 @@ func CalculateLabel(accountURI string) string {
 }
 
 func main() {
-	fmt.Println(CalculateLabel("https://example.com/acme/acct/ExampleAccount"))
+	// Parse the command line flags.
+	flag.Parse()
+	fmt.Println(CalculateLabel(*label))
 }
